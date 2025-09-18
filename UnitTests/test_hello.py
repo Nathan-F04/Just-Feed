2f0644b15d2af1df52231f4ef7fdf8c763a1ada3
Alex <G00424185@atu.ie>
2025-09-18 17:27:42 +0100

    Added TestFiles & UnitTest to test Workflows

--- UnitTests/test_hello.py
+++ UnitTests/test_hello.py
@@ -1,10 +1,14 @@
+"""Test module for hello functionality."""
 import unittest
 from io import StringIO
 import sys
+from TestFiles import hello
 
 class TestHelloWorld(unittest.TestCase):
+    """Test class for hello world functionality."""
+    
     def test_hello_output(self):
-        from TestFiles import hello
+        """Test hello output message."""
         captured_output = StringIO()
         sys.stdout = captured_output
         hello.print_message("Hello, world!")
