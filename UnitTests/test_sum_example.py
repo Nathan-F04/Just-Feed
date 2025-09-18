69f2b4544af16da62db2ad360ea153dd7a05adc7
ALEX PATON - STUDENT <g00424185@atu.ie>
2025-09-18 14:55:44 +0100

    Added TestFiles & UnitTest to test Workflows

---
+++ UnitTests/test_sum_example.py
@@ -0,0 +1,13 @@
+import unittest
+from TestFiles.hello import add
+
+class TestSumExample(unittest.TestCase):
+    def test_add_positive(self):
+        self.assertEqual(add(2, 3), 5)
+    def test_add_zero(self):
+        self.assertEqual(add(0, 0), 0)
+    def test_add_negative(self):
+        self.assertEqual(add(-1, -1), -2)
+
+if __name__ == "__main__":
+    unittest.main()
