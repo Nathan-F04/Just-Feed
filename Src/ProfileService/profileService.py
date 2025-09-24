8dc6457ac3320bacc2df216610af8f4253a24aef
NATHAN FERRY - STUDENT <g00413711@atu.ie>
2025-09-24 14:36:50 +0100

    added update details method

---
+++ Src/ProfileService/profileService.py
@@ -0,0 +1,14 @@
+# ProfileService/profileService.py
+from fastapi import FastAPI, HTTPException, status
+from .schemas import User
+
+ProfileService = FastAPI()
+profiles: list[Profile] = []
+
+@app.put("/api/profile/", status_code=status.HTTP_201_CREATED)
+def update_profile(profile: Profile):
+    for p in profiles:
+        if(p.profile_id == profile.profile_id):
+            profiles[profiles.index(p)] = profile 
+            return profile
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
