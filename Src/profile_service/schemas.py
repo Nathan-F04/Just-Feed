5d838938d8a7bf63bc1ce1a685c023ae3e1770c2
Shine Sujith <shinesujith25@gmail.com>
2025-10-03 15:16:08 +0100

    fixed linting issue

--- Src/profile_service/schemas.py
+++ Src/profile_service/schemas.py
@@ -2,9 +2,11 @@
 
 from pydantic import BaseModel, EmailStr, constr
 
+pattern_str = r"[a-z]"
+
 class Setting(BaseModel):
     """Json input vailidation for User"""
     account_id: int
     name: constr(min_length=2, max_length=25)
     email: EmailStr
-    password: constr(min_length=5, max_length=50, pattern = "[a-z]")
+    password: constr(min_length=5, max_length=50, pattern=pattern_str)
