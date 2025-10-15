fa18572b281977aa3a1eb6a86458f4e70481e59f
Nathan <G00413711@atu.ie>
2025-10-15 08:37:02 +0100

    finished tests, started put in banking

--- Src/banking_service/banking.py
+++ Src/banking_service/banking.py
@@ -1,11 +1,23 @@
 """Banking python file"""
 
-# app/main.py
-from fastapi import FastAPI, HTTPException, status
-from .schemas import Bank_user
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
+from .database import engine, SessionLocal
+from .models import Base, BankUserDB
+from .schemas import BankUserCreate, BankUserRead
 
 app = FastAPI()
-bank_details: list[Bank_user] = []
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/api/banking", response_model=list[BankUserRead])
 def get_all_bank_accounts(db: Session = Depends(get_db)):
@@ -31,8 +43,29 @@ def add_bank_account(payload: BankUserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=409, detail="bank_id already exists")
     return bank_user
 
-@app.put("/api/banking/{banking_id}", status_code=status.HTTP_200_OK)
+
+
+@app.put("/api/banking/{banking_id}", response_model=BankUserRead, status_code=status.HTTP_200_OK)
 def edit_bank_account_details(banking_id: int, bank_account: Bank_user):
+    for u in bank_details:
+        if u.banking_id == banking_id:
+            if banking_id == bank_account.banking_id:
+                bank_details[bank_details.index(u)] = bank_account
+                return bank_account
+            else:
+                return {"message" : "Can't update user_id value"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+@app.put("/api/banking/{banking_id}", response_model=BankUserRead, status_code=status.HTTP_200_OK)
+def edit_bank_account_details(banking_id: int, payload: BankUserCreate, db: Session = Depends(get_db)):
+    bank_user_changed = BankUserDB(**payload.model_dump())
+    bank_user_original = get_bank_account(banking_id)
+    # stmt = select(BankUserDB).where(BankUserDB.name == "patrick")
+    # patrick = session.scalars(stmt).one()
+    # patrick.addresses.append(Address(email_address="patrickstar@sqlalchemy.org"))
+    # sandy_address.email_address = "sandy_cheeks@sqlalchemy.org"
+
+    # db.commit()
     for u in bank_details:
         if u.banking_id == banking_id:
             if banking_id == bank_account.banking_id:
