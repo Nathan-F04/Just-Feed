0bce309368d010d82816613f618b953582c5b51c
Alex Paton <118891272+AlexP8on7@users.noreply.github.com>
2025-11-14 18:25:42 +0000

    Corrected code coverage

--- Src/order_service/order_tests/conftest.py
+++ Src/order_service/order_tests/conftest.py
@@ -2,24 +2,8 @@
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from Src.order_service.orders import app, get_db
-from Src.order_service.models import Base
-
-TEST_DB_URL = "sqlite+pysqlite:///:memory:"
-engine = create_engine(TEST_DB_URL, connect_args={"check_same_thread": False})
-TestingSessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
-Base.metadata.create_all(bind=engine)
+from Src.order_service.orders import app
 
 @pytest.fixture
 def client():
-    def override_get_db():
-        db = TestingSessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-    app.dependency_overrides[get_db] = override_get_db
-    with TestClient(app) as c:
-        yield c
+    return TestClient(app)
