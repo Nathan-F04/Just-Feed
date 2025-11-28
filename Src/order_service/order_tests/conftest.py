a1ff14ea1fe80720b3093d495baca301d83e87dd
ShineSujith <118687029+ShineSujith@users.noreply.github.com>
2025-11-28 16:10:59 +0000

    formated file structure and fixed make, ini and ci files

--- Src/order_service/order_tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Order test configs"""
-
-import pytest
-from fastapi.testclient import TestClient
-from Src.order_service.orders import app
-
-@pytest.fixture
-def client():
-    return TestClient(app)
